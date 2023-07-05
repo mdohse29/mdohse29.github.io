285f01e8ff4a46f510cffe7e8837913abeb2bd54
Michael <mdohse29@gmail.com>
2023-07-05 14:29:36 -0700

    Automatic Updates - Wed 05 Jul 2023 02:29:35 PM MST

---
+++ picker/picker_jq_docR.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("#display").css("height", $(window).height());
+	$("#display").css("width", $(window).width());
+
+$('button').attr('disabled', 'disabled');
+// $('button.list').removeAttr('disabled');
+$('#display').append("<p style=\"text-align: center\">Ohh, sorry! It is a little early.<br/>Come back in August 2023, The List will be online.</p>");
+// Remove ^^^^^ to activate the picker
+
+
+// $("#giftList").mouseleave(function(event) {
+// 	$(this).toggle("hide");
+// });
+
+	
+});
