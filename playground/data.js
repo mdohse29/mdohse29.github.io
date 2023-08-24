6176a386743db75dc5b6ecd900be10db3122030a
michaeldohse <74016819+michaeldohse@users.noreply.github.com>
2023-08-23 22:59:51 -0700

    Doing somthing stupid but cool

---
+++ playground/data.js
@@ -0,0 +1,15 @@
+const drawers = {
+    drawer1 : {
+        hidden: false,
+        drawerTitle: 'Title of the drawer',
+        item1: {
+            hidden: false,
+            url: '',
+            text: {
+                main: '',
+                sub: '',
+                warn: false
+            }
+        }
+    }
+}
