7fbef01a57bd95baa72499eaf586e50d8dac1258
Michael <mdohse29@gmail.com>
2023-08-26 23:18:02 -0700

    Trying to remove extra text from titles

--- misc/data/archiveData.js
+++ misc/data/archiveData.js
@@ -4,6 +4,20 @@ $.get('./data/archive.txt', function(data, status){
       $('p[id="placeH"]').remove();
     }
     for (a = 0; a < movies.length; a++){
-      $('.movies').append('<p class="title" style="background-color: none;font-weight: bold;font-size: 1em;">' + movies[a].replaceAll('.', ' ') + '</p>');
+      let formatted = formatter(movies[a]);
+      if (movies[a].match(/\[\d\d\d\d\]/g) || movies[a].match(/\(\d\d\d\d\)/g)){
+
+        // console.log(formatted.replace(/\[(\d\d\d\d)\].*/g, '($1)'));
+      $('.movies').append('<p class="title" style="background-color: none;font-weight: bold;font-size: 1em;">' + formatted + '</p>');
+      
+      }
     }
   }, 'text');
+
+  const formatter = function (text){
+    let format = text.replace(/(\(\d\d\d\d\)).*/g, '$1');
+    format = format.replace(/\[(\d\d\d\d)\].*/g, '($1)');
+    format = format.replaceAll('.', ' ');
+    format = format.replace(/([\w\d])(\()/g, '$1 $2');
+    return format;
+  }
