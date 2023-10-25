cbb7654f697f42f99612e85891e7154319c2eaa9
Michael <mdohse29@gmail.com>
2023-10-24 23:50:28 -0700

    Automatic Updates - Tue 24 Oct 2023 11:50:28 PM MST

--- playground/test.js
+++ playground/test.js
@@ -1,7 +1,58 @@
-axios.get("https://mdohse29.github.io/misc/data/archive.txt")
-.then( data => {
-    let titles = data.data.split("\n");
-    for (let i in titles){
-        console.log(titles[i]);
+// async function searchShows(title){
+//     const searchApi = await fetch('https://api.tvmaze.com/search/shows?q=' + title);
+//     const results = await searchApi.json();
+//     const presentation = document.querySelector('.results');
+//     for (let result of results){
+//     console.log(result.show.name);
+//     let p = document.createElement('p');
+//     // p.innerText = result.show.name;
+//     let src = result.show.image.medium;
+//     if(src){
+//     let img = document.createElement('img');
+//     img.src = src;
+//     p.appendChild(img);
+//     }else{
+//         p.innerText = result.show.name;
+//     }
+//     presentation.appendChild(p);
+//     }
+// }
+
+async function searchShows(title){
+    let options = {
+        params: {
+            q:title
+        }
     }
-})
+    await axios.get('https://api.tvmaze.com/search/shows', options).then(res => {
+        console.log(res.data);
+        const presentation = document.querySelector('.results');
+        for (let result of res.data){
+            console.log(result.show.name);
+            let p = document.createElement('p');
+            // p.innerText = result.show.name;
+            let src = result.show.image.medium;
+            if(src){
+                let img = document.createElement('img');
+                img.src = src;
+                p.appendChild(img);
+            }else{
+                p.innerText = result.show.name;
+            }
+            presentation.appendChild(p);
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+const search = document.querySelector('button');
+search.addEventListener('click', () => {
+    document.querySelector('.results').innerHTML = '';
+    let title = document.querySelector('#title').value;
+    if (title){
+        searchShows(title).then(() => {
+            console.log("ALL Done!");
+        });
+    }
+});
