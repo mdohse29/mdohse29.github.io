da0e3a738d90df22b8ce1c9a4d7da4d14eda5937
Michael <mdohse29@gmail.com>
2023-11-06 00:01:40 -0700

    CDN generator is working for LM2.0

--- linkMaker/data/jq_functions.js
+++ linkMaker/data/jq_functions.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
     $('#mediaselect').click(() => {
-        $('#linkbuild').append(generateInput('base','Base','Either course base (alg01) if in curric media or unit UUID if in unit folder'));
+        $('#linkbuild').append(generateInput('dataBasePath','Data Base Path (Unit UUID)','Either course base (alg01) if in curric media or unit UUID if in unit folder'));
     })
 
 });
