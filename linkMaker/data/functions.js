300336f5e946fc2c0e515b53b9fed47fb90a16ad
Michael <mdohse29@gmail.com>
2023-11-10 23:32:51 -0700

    Did more work on DLA, possible bug with pff on dla

--- linkMaker/data/functions.js
+++ linkMaker/data/functions.js
@@ -1,3 +1,12 @@
+// PFF is not rendering in preview, might not be able to include
+function space(text){
+    for(a = 0; a < text.length; a++){
+    if (text[a] == " "){
+        text = text.replace(' ', '%20');
+    }
+    }
+    return text;
+};
 function generateInput(id, text, title, ph){
     let div = document.createElement('div');
     let label = document.createElement('label');
@@ -54,84 +63,64 @@ function resetEverything(){
     toggleExtras();
 }
 
+function submitData(){
+    let textArea = document.querySelector('#textarea');
+    let mediaType = document.querySelector('#mediaType').value;
+    let url = document.createElement('p');
+
+    url.classList.add('has-text-centered');
+
+    if (textArea.querySelector('p')){
+        textArea.querySelector('p').remove();
+    }
+
+    textArea.classList.remove('dnone');
+
+    if (mediaType == 'DOC'){
+        mediaLink(url);
+    }else if (mediaType == 'DLA'){
+        dlaLink(url);
+    }else if (mediaType == 'Animation'){
+        animationLink(url);
+    }
+
+    if (url.innerHTML){
+        url.classList.add('has-background-success-light');
+        textArea.appendChild(url);
+    }
+}
+
 function toggleExtras(){
     let value = document.querySelector('#mediaType').value;
     if (value == "DOC"){
         document.querySelector('#pffCheck').parentElement.classList.add('dnone');
+
         document.querySelector('#text').parentElement.classList.add('dnone');
-    }else{
+
+    }/*else if (value == "DLA"){
+        document.querySelector('#text').parentElement.classList.remove('dnone');
+
+    }*/else{
         document.querySelector('#pffCheck').parentElement.classList.remove('dnone');
+
         document.querySelector('#text').parentElement.classList.remove('dnone');
     }
 }
 
-function animationLink(url){
-    let base = document.querySelector('#base').value;
-    let sourcePath = document.querySelector('#sourcePath').value;
-    let basePath = '';
-    let pff = '';
-    let topText = '';
-    let bottomText = '';
+function showPreview(){
+    let preview = document.querySelector('#preview');
+    let url = document.querySelector('#textarea > p').innerText;
 
-    url.innerText = 'https://cdn.lti.glynlyon.com/interactives/chm/cdn_harness/cdn_harness.html?base=' + base + '&file=' + sourcePath;
-
-    if (document.querySelector('#pffCheck').checked){
-        pff = document.querySelector('#pffFile').value;
-        if (!pff.includes('.pff')){
-            pff += '.pff';
-        }
-        basePath = document.querySelector('#dataBasePath').value;
-        if (!basePath && (base.length > 15 && base.includes('-'))){
-            basePath = base;
-        }
-        url.innerText += '&dataBasePath=' + basePath + '&pff=' + pff;
-    }
-
-    if (document.querySelector('#text').checked){
-        topText = document.querySelector('#topText').value;
-        bottomText = document.querySelector('#bottomText').value;
-        if (topText){
-            url.innerText += '&topText=' + topText;
-        }
-        if (bottomText){
-            url.innerText += '&bottomText=' + bottomText;
-        }
-    }
+    preview.classList.remove('dnone');
+    preview.querySelector('iframe').src = url;
 }
 
-function mediaLink(url){
-    let fileType = '';
-    let docType = '';
-    let dataBasePath = document.querySelector('#dataBasePath').value;
-    let fileName = document.querySelector('#fileName').value;
-
-    if (!dataBasePath){
-        dataBasePath = 'null';
-        url.classList.add('has-background-danger');
-    }
-    if (!fileName){
-        fileName = 'null';
-        url.classList.add('has-background-danger');
-    }else{
-        fileType = fileName.substring(fileName.lastIndexOf('.') + 1);
-    }
-
-    if (fileType == 'mp4' || fileType == 'webm' || fileType == 'flv'){
-        docType = 'video';
-    }else if (fileType == 'mp3'){
-        docType = 'audio';
-    }else if (fileType == 'jpg' || fileType == 'gif' || fileType == 'png'){
-        docType = 'img';
-    }else{
-        docType = 'misc';
-    }
-
-    url.innerHTML = 'https://cdn.lti.glynlyon.com/media/' + dataBasePath + '/' + docType + '/' + fileName;
+function closePreview(){
+    document.querySelector('#preview').querySelector('iframe').src = '';
+    document.querySelector('#preview').classList.add('dnone');
 }
 
-resetEverything();
-
-document.querySelector('#mediaselect').addEventListener('click', () => {
+function mediaSelect(){
     document.querySelector('#selectMenu').classList.add('dnone');
     let type = document.querySelector('#mediaType').value;
     let build = document.querySelector('#linkbuild');
@@ -148,46 +137,7 @@ document.querySelector('#mediaselect').addEventListener('click', () => {
 
     reset.addEventListener('click', resetEverything);
 
-    submit.addEventListener('click', () => {
-        let textArea = document.querySelector('#textarea');
-        let mediaType = document.querySelector('#mediaType').value;
-        let url = document.createElement('p');
-
-        url.classList.add('has-text-centered');
-
-        if (textArea.querySelector('p')){
-            textArea.querySelector('p').remove();
-        }
-
-        textArea.classList.remove('dnone');
-
-        if (mediaType == 'DOC'){
-            mediaLink(url);
-        }else if (mediaType == 'DLA'){
-            let source = document.querySelector('#source').value;
-            let dbp = document.querySelector('#dataBasePath').value;
-            let dlaFilename = document.querySelector('#dlaFilename').value;
-
-            if (!dlaFilename.includes('.dla')){
-                dlaFilename += '.dla';
-            }
-
-
-            if (document.querySelector('#pffCheck').checked){
-
-            }else{
-                // https://cdn.lti.glynlyon.com/interactives/chm/cdn_harness/cdn_harness.html?base=' + base + '&file=' + file + '&dataBasePath=' + dataBasePath + '&dlaFile=' + dlaFile + '.dla
-                url.innerHTML = 'https://cdn.lti.glynlyon.com/interactives/chm/cdn_harness/cdn_harness.html?base=global&file=' + source + '&dataBasePath=' + dbp + '&dlaFile=' + dlaFilename;
-            }
-        }else if (mediaType == 'Animation'){
-            animationLink(url);
-        }
-
-        if (url.innerHTML){
-            url.classList.add('has-background-success-light');
-            textArea.appendChild(url);
-        }
-    });
+    submit.addEventListener('click', submitData);
 
     if (type == "DOC"){
         build.appendChild(generateInput('dataBasePath','Unit UUID','The UUID for the unit can be found in Cayman.'));
@@ -236,19 +186,111 @@ document.querySelector('#mediaselect').addEventListener('click', () => {
 
     build.appendChild(submit);
     build.appendChild(reset);
-});
+}
 
-document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('#preview').querySelector('iframe').src = '';
-    document.querySelector('#preview').classList.add('dnone');
-});
+function animationLink(url){
+    let base = document.querySelector('#base').value;
+    let sourcePath = document.querySelector('#sourcePath').value;
+    let basePath = '';
+    let pff = '';
+    let topText = '';
+    let bottomText = '';
 
-document.querySelector('#showPreview').addEventListener('click', () => {
-    let preview = document.querySelector('#preview');
-    let url = document.querySelector('#textarea > p').innerText;
+    url.innerText = 'https://cdn.lti.glynlyon.com/interactives/chm/cdn_harness/cdn_harness.html?base=' + base + '&file=' + sourcePath;
 
-    preview.classList.remove('dnone');
-    preview.querySelector('iframe').src = url;
-});
+    if (document.querySelector('#pffCheck').checked){
+        pff = document.querySelector('#pffFile').value;
+        if (!pff.includes('.pff')){
+            pff += '.pff';
+        }
+        basePath = document.querySelector('#dataBasePath').value;
+        if (!basePath && (base.length > 15 && base.includes('-'))){
+            basePath = base;
+        }
+        url.innerText += '&dataBasePath=' + basePath + '&pff=' + pff;
+    }
+
+    if (document.querySelector('#text').checked){
+        topText = document.querySelector('#topText').value;
+        bottomText = document.querySelector('#bottomText').value;
+        if (topText){
+            url.innerText += '&topText=' + space(topText);
+        }
+        if (bottomText){
+            url.innerText += '&bottomText=' + space(bottomText);
+        }
+    }
+}
+
+function mediaLink(url){
+    let fileType = '';
+    let docType = '';
+    let dataBasePath = document.querySelector('#dataBasePath').value;
+    let fileName = document.querySelector('#fileName').value;
+
+    if (!dataBasePath){
+        dataBasePath = 'null';
+        url.classList.add('has-background-danger');
+    }
+    if (!fileName){
+        fileName = 'null';
+        url.classList.add('has-background-danger');
+    }else{
+        fileType = fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
+
+    if (fileType == 'mp4' || fileType == 'webm' || fileType == 'flv'){
+        docType = 'video';
+    }else if (fileType == 'mp3'){
+        docType = 'audio';
+    }else if (fileType == 'jpg' || fileType == 'gif' || fileType == 'png'){
+        docType = 'img';
+    }else{
+        docType = 'misc';
+    }
+
+    url.innerHTML = 'https://cdn.lti.glynlyon.com/media/' + dataBasePath + '/' + docType + '/' + fileName;
+}
+
+function dlaLink(url){
+    let source = document.querySelector('#source').value;
+    let dbp = document.querySelector('#dataBasePath').value;
+    let dlaFilename = document.querySelector('#dlaFilename').value;
+
+    if (!dlaFilename.includes('.dla')){
+        dlaFilename += '.dla';
+    }
+
+    url.innerHTML = 'https://cdn.lti.glynlyon.com/interactives/chm/cdn_harness/cdn_harness.html?base=global&file=' + source + '&dataBasePath=' + dbp + '&dlaFile=' + dlaFilename;
+
+    if (document.querySelector('#pffCheck').checked){
+        let pff = document.querySelector('#pffFile').value;
+        if (pff){
+            if (!pff.includes('.pff')){
+                pff += '.pff';
+            }
+            url.innerHTML += '&pff=' + pff;
+        }
+    }
+
+    if (document.querySelector('#text').checked){
+        let topText = document.querySelector('#topText').value;
+        let bottomText = document.querySelector('#bottomText').value;
+        if (topText){
+            url.innerHTML += '&topText=' + space(topText);
+        }
+        if (bottomText){
+            url.innerHTML += '&bottomText=' + space(bottomText);
+        }
+    }
+}
+
+resetEverything();
+
+document.querySelector('#mediaselect').addEventListener('click', mediaSelect);
+
+document.querySelector('.close').addEventListener('click', closePreview);
+
+document.querySelector('#showPreview').addEventListener('click', showPreview);
 
 document.querySelector('#mediaType').addEventListener('change', toggleExtras);
