8069c7770d33d7e6b340c8d9f32013344a7946d9
michaeldohse <74016819+michaeldohse@users.noreply.github.com>
2023-11-12 11:41:50 -0700

    Moms request

--- picker/picker_jq_docR.js
+++ picker/picker_jq_docR.js
@@ -15,9 +15,9 @@ $(document).ready(function(){
 		$('#giftList').toggle('fade',function(){
 			
 			$('.view').removeAttr('disabled');
-			if ($('.pick').attr('data-listcomplete') === 'false'){
-				$('.pick').removeAttr('disabled');
-			}
+			// if ($('.pick').attr('data-listcomplete') === 'false'){
+			// 	$('.pick').removeAttr('disabled');
+			// }
 		}, 1000);
 	});
 
@@ -32,21 +32,21 @@ $(document).ready(function(){
 
 	});
 
-	$('button').attr('disabled', 'disabled');
-	$('#display').append("<p style=\"text-align: center\" id=\"erlmsg\">Ohh, sorry! It is a little early.<br/>Come back in September 2024, The List will be online.</p>");
+	// $('#select button').attr('disabled', 'disabled');
+	// $('#display').append("<p style=\"text-align: center\" id=\"erlmsg\">Ohh, sorry! It is a little early.<br/>Come back in September 2024, The List will be online.</p>");
 	// Comment out ^^^^^ to activate the picker
 
 	// $('button.list').removeAttr('disabled');
 	//// Activate list button only ^^^^^^
 	
-	if ((currentMonth == 11 && currentDay <= 22) || (currentMonth > 7 && currentMonth < 11)){
-		$('button').removeAttr('disabled');
+	// if ((currentMonth == 11 && currentDay <= 22) || (currentMonth > 7 && currentMonth < 11)){
+	// 	$('button').removeAttr('disabled');
 		
-		$('#erlmsg').remove();
-		setTimeout(function(){
-			viewList();
-		}, 700);
-	}
+	// 	$('#erlmsg').remove();
+		// setTimeout(function(){
+		// 	viewList();
+		// }, 700);
+	// }
 	// Activates the picker from September 01 thru December 22 ^^^^^
 
 	// $('#btn-add-lst').attr('disabled', 'disabled');
