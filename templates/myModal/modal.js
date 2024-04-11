5f60d2821f36c1d0179e76604d619ae05017fdf8
michaeldohse <74016819+michaeldohse@users.noreply.github.com>
2024-04-10 22:21:34 -0700

    Created modal template, still work in progress but usable

---
+++ templates/myModal/modal.js
@@ -0,0 +1,7 @@
+document.querySelector('#info').addEventListener('click', () => {
+    document.querySelector('.md-modal').classList.remove('dnone');
+});
+
+document.querySelector('.md-modal-background').addEventListener('click', () => {
+    document.querySelector('.md-modal').classList.add('dnone');
+});
