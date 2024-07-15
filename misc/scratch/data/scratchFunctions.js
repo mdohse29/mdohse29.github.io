6607227a8104b29b77f61400a9cd15fa05c0a976
michaeldohse <74016819+michaeldohse@users.noreply.github.com>
2024-07-15 08:49:29 -0700

    Fixing bug with new regex for rmv OL

--- misc/scratch/data/scratchFunctions.js
+++ misc/scratch/data/scratchFunctions.js
@@ -163,7 +163,7 @@ $(document).ready(function(){
     }
 
     function removeOlMarkers(text){
-        let singleDigit = [/^ *[A-Z]{1,4}[).] *\b/gm, /^ *[a-z]{1,4}[).] *\b/gm, /^ *[0-9]{1,4}[).] *\b/gm]
+        let singleDigit = [/^[ \t]*[A-Z]{1,4}[).][ \t]*\b/gm, /^[ \t]*[a-z]{1,4}[).][ \t]*\b/gm, /^[ \t]*[0-9]{1,4}[).][ \t]*\b/gm]
         let editedText = text;
         for (let a in singleDigit){
             // console.log(singleDigit[a]);
