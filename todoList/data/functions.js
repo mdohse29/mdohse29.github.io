f5cb36aa8b21b310da9f1f7990af1726b5ca3652
michaeldohse <74016819+michaeldohse@users.noreply.github.com>
2024-08-09 10:24:52 -0700

    Fixed a little bug

--- todoList/data/functions.js
+++ todoList/data/functions.js
@@ -83,11 +83,15 @@ document.querySelector('#item').addEventListener('input', function(){
         if (itemText){
             // alert(itemText.substring(0, itemText.length - 1))
             navigator.clipboard.writeText(itemText.substring(0, itemText.length - 1));
+            let submit = document.querySelector('#submit');
+
+            submit.setAttribute('disabled', 'disabled');
             this.classList.add('is-success');
             this.value = 'Export Complete!';
             setTimeout(() => {
                 this.value = '';
-                this.classList.remove('is-success')
+                this.classList.remove('is-success');
+                submit.removeAttribute('disabled');
             }, 1000);
         }
     }
