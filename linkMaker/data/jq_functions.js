fd1cf9d394df3f233b3dfa9e66123c018144558e
MasterPC <mdohse29@gmail.com>
2024-10-12 06:45:58 -0700

    Not being used - Sat Oct 12 06:45:58 AM MST 2024

--- linkMaker/data/jq_functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-
-    $('#mediaselect').click(() => {
-        $('#linkbuild').append(generateInput('dataBasePath','Data Base Path (Unit UUID)','Either course base (alg01) if in curric media or unit UUID if in unit folder'));
-    })
-
-});
