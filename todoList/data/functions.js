1153e6b378778b39608e1d1b22c446d27c1d66f9
MR D <mdohse29@gmail.com>
2025-02-04 17:58:56 -0700

    Automatic Updates - Tue Feb  4 05:58:55 PM MST 2025

--- todoList/data/functions.js
+++ todoList/data/functions.js
@@ -640,7 +640,8 @@ function closeOptions(){
 
     let options = document.querySelector('.options');
 
-    options.remove();
+    if (options)
+        options.remove();
 
 }
 
@@ -655,12 +656,12 @@ function openOptions(event){
         b3:mkBtn({class:'button is-small ml-2 mr-2 is-rounded is-warning is-outlined', 'aria-description':'Edit List Item', id:'edit', inner:'<i class="bi bi-pencil"></i>', listeners:[{type:'click', execute:toggleLstBtn}, {type:'mouseenter', execute:optMo}, {type:'mouseleave', execute:optMl}]}),
         b4:mkBtn({class:'button is-small ml-2 mr-2 is-rounded is-info is-outlined', 'aria-description':'Copy Item', id:'copy', inner:'<i class="bi bi-copy"></i>', listeners:[{type:'click', execute:clkCopyItem}, {type:'mouseenter', execute:optMo}, {type:'mouseleave', execute:optMl}]}),
         b5:mkBtn({class:'button is-small ml-2 mr-2 is-rounded is-warning is-outlined', 'aria-description':'Restore List Item', id:'undo', inner:'<i class="bi bi-arrow-counterclockwise"></i>', listeners:[{type:'click', execute:clkUndoItem}, {type:'mouseenter', execute:optMo}, {type:'mouseleave', execute:optMl}]}),
-        b6:mkBtn({class:'button is-small ml-2 mr-2 is-rounded is-danger is-outlined', 'aria-description':'Close Options', id:'cancel', inner:'<i class="bi bi-x-circle"></i>', listeners:[{type:'click', execute:closeOptions}, {type:'mouseenter', execute:optMo}, {type:'mouseleave', execute:optMl}]})
+        b6:mkBtn({class:'button is-small ml-2 mr-2 is-rounded is-danger is-outlined', 'aria-description':'Close Options', id:'cancel', inner:'<i class="bi bi-x-circle"></i>', listeners:[{type:'click', execute:()=>{closeOptions();targetElement=null;}}, {type:'mouseenter', execute:optMo}, {type:'mouseleave', execute:optMl}]})
     }
 
     nestElem([
-        document.querySelector('.container'),
-        mkDiv({class:'options', style:'top: ' + (event.y - 30) + 'px; left: ' + (event.x - 27) + 'px;'}),
+        document.querySelector('.card'),
+        mkDiv({class:'options', style:'top: ' + (event.y - 30) + 'px; left: ' + (event.x - 27) + 'px;', listeners:[{type:'mouseleave', execute:() => {closeOptions();targetElement=null;}}]}),
         mkDiv({class:'card'}),
         mkDiv({class:'card-content p-0'}),
         (targetElement.parentElement.id === 'done'||targetElement.parentElement.parentElement.id === 'done')?
